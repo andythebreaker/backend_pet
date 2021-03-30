541224133c03929586e42335412b4dcc3f46db2b
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-03-31 04:19:58 +0800

    From the layout, there are three pages, index(member) \ register \ login Next, add routing so that you can switch to the specified page normally when you click on the link  Modify users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//加入register routing
+router.get('/register', function(req, res, next) {
+  res.render('register', {title: 'Register'});
+});
+
+//加入login routing
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
 module.exports = router;
